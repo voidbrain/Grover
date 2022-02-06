e9692032263ffe50a9e9b14c80b8c89f2e538d0f
voidbrain <daniele.bordignon@gmail.com>
2022-02-07 00:42:10 +0100

    ds18x20 + l298n ok

---
+++ testL298NOk.js
@@ -0,0 +1,17 @@
+'use strict';
+const L298N = require('pigpio-l298n');
+
+// enableA,in1,in2,enableB,in3,in4
+let l298n = new L298N(14,15,18, 21,20,16);
+l298n.setSpeed(l298n.NO1,100); // 20%
+l298n.forward(l298n.NO1);
+l298n.setSpeed(l298n.NO2,100); // 20%
+l298n.forward(l298n.NO2);
+// l298n.backward(l298n.NO1)
+
+setTimeout(() => {
+  l298n.stop(l298n.NO1);
+  l298n.stop(l298n.NO2);
+  console.log('done');
+}, 2000); 
+
