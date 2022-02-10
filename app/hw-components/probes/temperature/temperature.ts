3456b8ca21859870587ec43fbc0142b698a62173
voidbrain <daniele.bordignon@gmail.com>
2022-02-10 23:54:30 +0100

    Action execution on schedule

--- app/hw-components/probes/temperature/temperature.ts
+++ app/hw-components/probes/temperature/temperature.ts
@@ -3,6 +3,11 @@ import { CronJobInterface } from '../../../interfaces/cron-job';
 import sensor from 'ds18x20';
 import schedule from 'node-schedule';
 
+const Owner = {
+  schedule: 'schedule',
+  user: 'user'
+};
+
 class TemperatureComponent {
   id: string;
   parentId: string;
@@ -13,15 +18,19 @@ class TemperatureComponent {
     this.id = id;
     this.parentId = parentId;
     this.scheduleArr = scheduleArr;
+
     this.setSchedule(this.id, this.parentId, this.scheduleArr);
   }
 
-  async read() {
+  async read(owner) {
     const self = this;
     return new Promise(resolve => {
       sensor.get(self.id, function (err: any, tempObj: any) {
         if (err) { throw err; }
-        resolve(tempObj);
+        // resolve(tempObj);
+        
+        console.log("==>", owner, tempObj, 
+        self.parentId, self.id, new Date());
       });
     });
   }
@@ -30,7 +39,8 @@ class TemperatureComponent {
     if(scheduleArr !== undefined) {
       scheduleArr.map(job => {
         schedule.scheduleJob(job.cron, () => {
-          console.log("==>", parentId, id, new Date(), job.cron, job.action, job.options);
+          const options = JSON.stringify(job.options); 
+          const result = eval(`this.${job.action}('${Owner.schedule}')`)
         });
       });
     } else {
