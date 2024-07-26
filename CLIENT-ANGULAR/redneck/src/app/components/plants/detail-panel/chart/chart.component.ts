ad7edeeaadd46d45c7f7332151b4e9c7eab206bd
daniele bordignon <daniele.bordignon@koerber.com>
2024-07-26 11:38:02 +0200

    Charts

--- CLIENT-ANGULAR/redneck/src/app/components/plants/detail-panel/chart/chart.component.ts
+++ CLIENT-ANGULAR/redneck/src/app/components/plants/detail-panel/chart/chart.component.ts
@@ -154,19 +154,6 @@ export class PanelChartComponent implements OnChanges {
 
     this.chartType = 'line';
 
-    this.chartData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'My First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
-        }
-      ]
-    };
-
     const chartOptions: ChartOptions = {
       plugins: {
         legend: { display: false },
